4e5820ef37566cf21c9814c4385ec2de563cac79
Gizem Deniz <gizemdeniz@Cem-MacBook-Pro.local>
2020-12-05 19:39:06 +0300

    design change

--- src/components/projects/iframes/index.jsx
+++ src/components/projects/iframes/index.jsx
@@ -12,25 +12,25 @@ const projectList = [
   <iframe
     src="https:/feed-the-needy.netlify.app/"
     height="500"
-    width="80%"
+    width="100%"
     title="M.Y-Inventory"
   ></iframe>,
     <iframe
     src="https://gizemportfolio.netlify.app/"
     height="500"
-    width="80%"
+    width="100%"
     title="Weekly-Workout-Planner"
   ></iframe>,
   <iframe
     src="https://movieprojectr.netlify.app/"
     height="500"
-    width="80%"
+    width="100%"
     title="It's On Us"
   ></iframe>,
   <iframe
-    src="https://madlibsrecoded.netlify.app"
+    src="https://madlibsrecoded.netlify.app/"
     height="500"
-    width="80%"
+    width="100%"
     title="Movie-Project"
   ></iframe>
 ];
